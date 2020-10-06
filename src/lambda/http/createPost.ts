52e7deb55ff01598ade451908e13ad82d8691d03
marcomcnultycodes <marcomcnultycodes@gmail.com>
2020-10-07 00:39:06 +0100

    📦 NEW: create post handler and serverless config

---
+++ src/lambda/http/createPost.ts
@@ -0,0 +1,50 @@
+import * as uuid from 'uuid';
+import * as AWS from 'aws-sdk';
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export const handler = async event => {
+  console.log(`Processing event: ${event}`);
+
+  const data = JSON.parse(event.body);
+  console.log(`New post: ${data}`);
+
+  const params = {
+    TableName: process.env.POSTS_TABLE,
+    Item: {
+      // Cognito Identity Pool identity id
+      userId: event.requestContext.identity.cognitoIdentityId,
+      postId: uuid.v4(),
+      title: data.title,
+      content: data.content,
+      createdAt: new Date().toISOString(),
+    },
+  };
+
+  // response headers enabling CORS
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  };
+
+  try {
+    await dynamoDb.put(params).promise();
+
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify(params.Item),
+    };
+  } catch (err) {
+    alert(`Error: ${err}`);
+
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ status: false }),
+    };
+  }
+};
